a5e092ed94f27e9f6ce83d4a16c71bf5e2d7fc3d
Łukasz Bazior <bandzior157@gmail.com>
2020-08-26 21:16:08 +0200

    Add tests target

---
+++ RickAndMortyCharactersApp/RickAndMortyCharactersAppTests/RickAndMortyCharactersAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  RickAndMortyCharactersAppTests.swift
+//  RickAndMortyCharactersAppTests
+//
+//  Created by Łukasz Bazior on 26/8/20.
+//  Copyright © 2020 Łukasz Bazior. All rights reserved.
+//
+
+import XCTest
+
+class RickAndMortyCharactersAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
